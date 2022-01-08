50de3c8e1bff983821514ee0d98d20e17aa979f8
Gilbertoaleite <gilbertoaleite@hotmail.com>
2022-01-08 15:23:54 -0300

    firt commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+export const config = {
+	API_URL: 'http://localhost:5005',
+
+	MONGO_CONNECTION:
+		//'mongodb+srv://<user>:<password>@new-url.lhwdp.mongodb.net/<cluster-name>?retryWrites=true&w=majority',
+		'mongodb+srv://gilbertoaleite:iaCG1VHvx0h6eAU4@encurtadorurl-dio.ukcrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+};
