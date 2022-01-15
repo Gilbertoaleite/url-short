dc90887c07a6cabd0481b32d280d7060c5b019c7
Gilbertoaleite <gilbertoaleite@hotmail.com>
2022-01-14 23:12:48 -0300

    Atualizando a conexão com o banco de dados

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -5,5 +5,5 @@ export const config = {
 
 	MONGO_CONNECTION:
 		//'mongodb+srv://<user>:<password>@new-url.lhwdp.mongodb.net/<cluster-name>?retryWrites=true&w=majority',
-		'mongodb+srv://gilbertoaleite:iaCG1VHvx0h6eAU4@encurtadorurl-dio.ukcrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
 };
